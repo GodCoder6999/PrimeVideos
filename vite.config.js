965ae9f36e5acd732a7d08236980e2aa45bedfbe
GodCoder6999 <casuala459@gmail.com>
2026-02-04 01:44:30 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://livesport.su',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
 })
