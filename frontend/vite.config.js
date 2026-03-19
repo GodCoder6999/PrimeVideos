deafafd4f029479a58d43c0d73b63cf717b4807a
Anirban Bose <casuala459@gmail.com>
2026-03-20 01:21:43 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,13 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
       '/api': {
-        target: 'https://livesport.su',
+        target: 'http://localhost:3000', // Point this to your local backend server
         changeOrigin: true,
         secure: false,
       },
